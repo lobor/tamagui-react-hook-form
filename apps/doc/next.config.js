cfa683c8365d04dde885781806d378da6950e521
Lionel Bertrand <lionel.bertrand@paymium.com>
2023-04-29 16:33:01 +0200

    add base path

--- apps/doc/next.config.js
+++ apps/doc/next.config.js
@@ -24,6 +24,7 @@ module.exports = function (name, { defaultConfig }) {
     images: {
       unoptimized: true,
     },
+    basePath: process.env.NODE_ENV === 'development' ? '/' : '/tamagui-react-hook-form/',
     assetPrefix: process.env.NODE_ENV === 'development' ? '/' : '/tamagui-react-hook-form/',
     output: 'export',
     modularizeImports: {
